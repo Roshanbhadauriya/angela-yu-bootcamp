4f0aeb9f9965b5b48c23a2667bbe83b884808765
Roshan Singh bhadauriya <75900678+Roshanbhadauriya@users.noreply.github.com>
2023-08-03 20:14:35 +0530

    aug 3
    Made my first game dice game

---
+++ 15.0+randomnumber/script.js
@@ -0,0 +1,6 @@
+prompt("enter your name");
+prompt("enter your crush name");
+
+let a =Math.random();
+a=a*100;
+console.log(Math.floor*a+1);
---
+++ 17.Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,20 @@
+let randomNumber1 ;
+randomNumber1 = Math.floor((Math.random() * 6) +1);
+
+document.querySelector(".img1").setAttribute("src",'images/dice'+randomNumber1 +'.png');
+
+
+let randomNumber2 ;
+randomNumber2 = Math.floor((Math.random() * 6) +1);
+
+document.querySelector(".img2").setAttribute("src",'images/dice'+randomNumber2 +'.png');
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player1 Wins 🚩"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player2 Wins 🚩"
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw 📍"
+}
